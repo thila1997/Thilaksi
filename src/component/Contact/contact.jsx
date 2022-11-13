5b98b58176455a297e9f093628541bec956d29b6
Thilaksi Gunathilaka <4thilagunathilaka@gmail.com>
2022-11-13 12:11:01 +0530

    CV  pdf add

--- src/component/Contact/contact.jsx
+++ src/component/Contact/contact.jsx
@@ -3,8 +3,22 @@ import './contact.css'
 import {AiOutlineMail} from 'react-icons/ai'
 import {FaTwitter} from 'react-icons/fa'
 import {IoLogoWhatsapp} from 'react-icons/io'
+import React, { useRef } from 'react';
+import emailjs from 'emailjs-com'
 
 const contact = () => {
+  const form = useRef();
+
+  const sendEmail = (e) => {
+    e.preventDefault();
+
+    emailjs.sendForm('service_5mswf59', 'template_aazbh8y', form.current, 'cwkTGeq1tulRsKMGP')
+      .then((result) => {
+          console.log(result.text);
+      }, (error) => {
+          console.log(error.text);
+      });
+  };
   return (
     <section id='contact'>
       <h5>Get In Touch</h5>
@@ -32,7 +46,7 @@ const contact = () => {
           </article>
         </div>
         {/* End contact option */}
-        <form action=''>
+        <form ref={form} onSubmit={sendEmail}>
           <input type="text" name='name' placeholder='Your Full Name' required />
           <input type="text" name='email' placeholder='Your Email' required />
           <textarea name='message' rows='7' placeholder='Your Message' required></textarea>
